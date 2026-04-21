67558a6f39ab0114a76949d64e42644615b96982
l1ght345 <kjpty7jkart2@mail.ru>
2026-04-21 17:31:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,119 @@
 import pygame
+from random import randint
+
 
 class Sprite():
-    def __init__(self,center,image):
+    def __init__(self, center, image):
         self.image = image
-        self.rect = image.get_frect(center = center)
-    
+        self.rect = image.get_frect(center=center)
+
     def render(self):
-        surface.blit(self.image,self.rect)
-    
+        surface.blit(self.image, self.rect)
+
+
 class Player(Sprite):
-    def __init__(self,center,image,btn_up,btn_down,speed):
-        super().__init__(center,image)
+    def __init__(self, center, image, btn_up, btn_down, speed):
+        super().__init__(center, image)
         self.btn_up = btn_up
         self.btn_down = btn_down
-        self.speed = speed  
-    
+        self.speed = speed
+
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[self.btn_up]:
             self.rect.y -= self.speed
-        if keys[self.btn_down]:            
-            self.rect.y += self.speed   
+        if keys[self.btn_down]:
+            self.rect.y += self.speed
         if self.rect.top < 0:
             self.rect.top = 0
         if self.rect.bottom > 600:
-            self.rect.bottom = 600         
+            self.rect.bottom = 600
+
+
+class Ball(Sprite):
+    def __init__(self, center, image, speed):
+        super().__init__(center, image)
+        self.speed = speed
+        self.direction = pygame.Vector2(1, 0)
+
+    def update(self):
+        self.rect.move_ip(self.direction * self.speed)
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.direction.y = -self.direction.y
+        if self.rect.bottom > 600:
+            self.rect.bottom = 600
+            self.direction.y = -self.direction.y
+
+
+def restart():
+    ball.rect.center = [400, 300]
+    ball.speed = 7
+    ball.direction.update(1, 0)
+    left_player.rect.center = [780, 300]
+    right_player.rect.center = [20, 300]
+
 
 pygame.init()
 
 window = pygame.Window("Ping-Pong", [800, 600])
 surface = window.get_surface()
 clock = pygame.Clock()
+font = pygame.sysfont.SysFont('Ariel',32)
 
 pygame.mixer.music.load('music.ogg')
-pygame.mixer.music.get_volume(0.1)
+pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
 
 player_image = pygame.image.load('rocket.png').convert_alpha()
-player_image = pygame.transform.scale(player_image,[20,100])
-right_player = Player([20,300],player_image,pygame.K_w,pygame.K_s,12)
-left_player = Player([780,300],player_image,pygame.K_UP,pygame.K_DOWN,12)
+player_image = pygame.transform.scale(player_image, [20, 100])
+right_player = Player([20, 300], player_image, pygame.K_w, pygame.K_s, 12)
+left_player = Player([780, 300], player_image, pygame.K_UP, pygame.K_DOWN, 12)
+
+ball_image = pygame.image.load("ball.png").convert_alpha()
+ball_image = pygame.transform.scale(ball_image, [50, 50])
+ball = Ball([400, 300], ball_image, 7)
 
 background_image = pygame.image.load('background.png').convert_alpha()
-background_image = pygame.transform.scale(background_image,[800,600])
+background_image = pygame.transform.scale(background_image, [800, 600])
 
+score = {'left': 0, 'right': 0}
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.WINDOWCLOSE:
             running = False
+
     right_player.update()
     left_player.update()
+    ball.update()
+    if right_player.rect.colliderect(ball.rect):
+        ball.direction.x = -ball.direction.x
+        ball.direction.rotate_ip(randint(-20, 20))
+        ball.speed += 0.5
 
+    if left_player.rect.colliderect(ball.rect):
+        ball.direction.x = -ball.direction.x
+        ball.direction.rotate_ip(randint(-20, 20))
+        ball.speed += 0.5
+
+    if ball.rect.left > 800:
+        score['left'] += 1
+        restart()
+    if ball.rect.right < 0:
+        score['right'] += 1
+        restart()
     surface.fill("white")
-    surface.blit(background_image,[0,0])
+    surface.blit(background_image, [0, 0])
 
     right_player.render()
     left_player.render()
+    ball.render()
+
+    sc1 = f'{score["left"]}|{score["right"]}'
+    sc2 = font.render(sc1,True,'white')
+    surface.blit(sc2,sc2.get_frect(midtop = [400,10]))
+
     window.flip()
     clock.tick(60)
