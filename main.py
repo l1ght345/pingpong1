06776648d60dfac7e8ca4daefbee3135e4d4379a
l1ght345 <kjpty7jkart2@mail.ru>
2026-04-28 17:32:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from random import randint
 
+import pygame.__briefcase
+
 
 class Sprite():
     def __init__(self, center, image):
@@ -30,6 +32,21 @@ class Player(Sprite):
             self.rect.bottom = 600
 
 
+class AutoPlayer(Sprite):
+    def __init__(self, center, image, speed):
+        super().__init__(center, image)
+        self.speed = speed
+    
+    def update(self, ball):
+        if ball.rect.centery > self.rect.bottom:
+            self.rect.y += self.speed
+        if ball.rect.centery < self.rect.top:
+            self.rect.y -= self.speed
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > 600:
+            self.rect.bottom = 600
+
 class Ball(Sprite):
     def __init__(self, center, image, speed):
         super().__init__(center, image)
@@ -46,14 +63,118 @@ class Ball(Sprite):
             self.direction.y = -self.direction.y
 
 
-def restart():
+def restart(ball,left_player,right_player):
     ball.rect.center = [400, 300]
     ball.speed = 7
     ball.direction.update(1, 0)
     left_player.rect.center = [780, 300]
     right_player.rect.center = [20, 300]
 
+def pvp():
+    player_image = pygame.image.load('rocket.png').convert_alpha()
+    player_image = pygame.transform.scale(player_image, [20, 100])
+    right_player = Player([20, 300], player_image, pygame.K_w, pygame.K_s, 12)
+    left_player = Player([780, 300], player_image, pygame.K_UP, pygame.K_DOWN, 12)
 
+    ball_image = pygame.image.load("ball.png").convert_alpha()
+    ball_image = pygame.transform.scale(ball_image, [50, 50])
+    ball = Ball([400, 300], ball_image, 7)
+
+    score = {'left': 0, 'right': 0}
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.WINDOWCLOSE:
+                running = False
+
+        right_player.update()
+        left_player.update()
+        ball.update()
+        if right_player.rect.colliderect(ball.rect):
+            ball.direction.x = -ball.direction.x
+            ball.direction.rotate_ip(randint(-20, 20))
+            ball.speed += 0.5
+
+        if left_player.rect.colliderect(ball.rect):
+            ball.direction.x = -ball.direction.x
+            ball.direction.rotate_ip(randint(-20, 20))
+            ball.speed += 0.5
+
+        if ball.rect.left > 800:
+            sound_hit.play()
+            score['left'] += 1
+            restart(ball,left_player,right_player)
+        if ball.rect.right < 0:
+            sound_hit.play()
+            score['right'] += 1
+            restart(ball,left_player,right_player)
+        surface.fill("white")
+        surface.blit(background_image, [0, 0])
+
+        right_player.render()
+        left_player.render()
+        ball.render()
+
+        sc1 = f'{score["left"]}|{score["right"]}'
+        sc2 = font.render(sc1,True,'white')
+        surface.blit(sc2,sc2.get_frect(midtop = [400,10]))
+
+        window.flip()
+        clock.tick(60)
+
+def pve():
+    player_image = pygame.image.load('rocket.png').convert_alpha()
+    player_image = pygame.transform.scale(player_image, [20, 100])
+    right_player = Player([20, 300], player_image, pygame.K_w, pygame.K_s, 12)
+    left_player = AutoPlayer([780, 300], player_image, 12)
+
+    ball_image = pygame.image.load("ball.png").convert_alpha()
+    ball_image = pygame.transform.scale(ball_image, [50, 50])
+    ball = Ball([400, 300], ball_image, 7)
+
+    score = {'left': 0, 'right': 0}
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.WINDOWCLOSE:
+                running = False
+
+        right_player.update()
+        left_player.update(ball)
+        ball.update()
+        if right_player.rect.colliderect(ball.rect):
+            ball.direction.x = -ball.direction.x
+            ball.direction.rotate_ip(randint(-20, 20))
+            ball.speed += 0.5
+
+        if left_player.rect.colliderect(ball.rect):
+            ball.direction.x = -ball.direction.x
+            ball.direction.rotate_ip(randint(-20, 20))
+            ball.speed += 0.5
+
+        if ball.rect.left > 800:
+            sound_hit.play()
+            score['left'] += 1
+            restart(ball,left_player,right_player)
+        if ball.rect.right < 0:
+            sound_hit.play()
+            score['right'] += 1
+            restart(ball,left_player,right_player)
+        surface.fill("white")
+        surface.blit(background_image, [0, 0])
+
+        right_player.render()
+        left_player.render()
+        ball.render()
+
+        sc1 = f'{score["left"]}|{score["right"]}'
+        sc2 = font.render(sc1,True,'white')
+        surface.blit(sc2,sc2.get_frect(midtop = [400,10]))
+
+        window.flip()
+        clock.tick(60)    
 pygame.init()
 
 window = pygame.Window("Ping-Pong", [800, 600])
@@ -65,55 +186,42 @@ pygame.mixer.music.load('music.ogg')
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
 
-player_image = pygame.image.load('rocket.png').convert_alpha()
-player_image = pygame.transform.scale(player_image, [20, 100])
-right_player = Player([20, 300], player_image, pygame.K_w, pygame.K_s, 12)
-left_player = Player([780, 300], player_image, pygame.K_UP, pygame.K_DOWN, 12)
-
-ball_image = pygame.image.load("ball.png").convert_alpha()
-ball_image = pygame.transform.scale(ball_image, [50, 50])
-ball = Ball([400, 300], ball_image, 7)
+sound_hit = pygame.Sound('hit.mp3')
+sound_hit.set_volume(0.3)
 
 background_image = pygame.image.load('background.png').convert_alpha()
 background_image = pygame.transform.scale(background_image, [800, 600])
 
-score = {'left': 0, 'right': 0}
-running = True
+button_image = pygame.image.load('button.jpg').convert_alpha()
+button_pvp_image = pygame.transform.scale(button_image, [200, 50])
+button_pve_image = button_pvp_image.copy()
 
+txt = font.render('PVP',True,'black')
+button_pvp_image.blit(txt,txt.get_rect(center = [100,25]))
+
+txt = font.render('PVE',True,'black')
+button_pve_image.blit(txt,txt.get_rect(center = [100,25]))
+
+button_pvp = Sprite([400,250],button_pvp_image)
+button_pve = Sprite([400,350],button_pve_image)
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.WINDOWCLOSE:
             running = False
-
-    right_player.update()
-    left_player.update()
-    ball.update()
-    if right_player.rect.colliderect(ball.rect):
-        ball.direction.x = -ball.direction.x
-        ball.direction.rotate_ip(randint(-20, 20))
-        ball.speed += 0.5
-
-    if left_player.rect.colliderect(ball.rect):
-        ball.direction.x = -ball.direction.x
-        ball.direction.rotate_ip(randint(-20, 20))
-        ball.speed += 0.5
-
-    if ball.rect.left > 800:
-        score['left'] += 1
-        restart()
-    if ball.rect.right < 0:
-        score['right'] += 1
-        restart()
-    surface.fill("white")
-    surface.blit(background_image, [0, 0])
-
-    right_player.render()
-    left_player.render()
-    ball.render()
-
-    sc1 = f'{score["left"]}|{score["right"]}'
-    sc2 = font.render(sc1,True,'white')
-    surface.blit(sc2,sc2.get_frect(midtop = [400,10]))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if button_pvp.rect.collidepoint(pygame.mouse.get_pos()):
+                    sound_hit.play()
+                    pvp()
+                if button_pve.rect.collidepoint(pygame.mouse.get_pos()):
+                    sound_hit.play()
+                    pve()
+    surface.blit(background_image,(0,0))
+    
+    button_pve.render()
+    button_pvp.render()
 
     window.flip()
     clock.tick(60)
